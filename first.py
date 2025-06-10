9bc76830f180d0451229c48aa63383d3e650a6da
jiyoung <muneuy311@naver.com>
2025-06-10 18:07:02 +0900

    연습

---
+++ first.py
@@ -0,0 +1,16 @@
+print('first module입니다')
+def my_func():
+    print('hello')
+
+def my_func1():
+    print('2nd hello')
+
+def my_func2():
+    print('3rd hello 나올까?')
+
+print('the end')
+print('hello')
+
+# run module
+if __name__ == '__main__' :
+    my_func()
---
+++ my_first_package/second.py
@@ -0,0 +1,12 @@
+print('first module입니다')
+def my_func():
+    print('hello')
+
+def my_func1():
+    print('2nd hello')
+
+def my_func2():
+    print('3rd hello 나올까?')
+
+
+print('last module입니다')
