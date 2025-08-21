3fff63b186e21aedb6cde572af69a30ee4c0344b
jiyoung <muneuy311@naver.com>
2025-08-21 11:53:50 +0900

    img

--- app/프로젝트_프로토타입.py
+++ app/프로젝트_프로토타입.py
@@ -17,7 +17,7 @@ st.title('**AutocarZ - 자율주행 중 로드킬 안전 강화 및 자동 신�
 
 ## 사이드바
 
-img_4 = Image.open('오또카지로고.png')
+img_4 = Image.open('logo.png')
 
 st.sidebar.image(img_4)
 st.sidebar.header('프로토타입')
@@ -25,7 +25,7 @@ st.sidebar.header('프로토타입')
 select_side = st.sidebar.selectbox('프로토타입', 
                                     ['카메라', '객체 인식', '전광판', 'Map & 통계'])
 
-img_1 = Image.open('고라니.jpg')
+img_1 = Image.open('gorani.jpg')
 img_2 = Image.open('yolo.png')
 img_3 = Image.open('고라니인식.png')
 
